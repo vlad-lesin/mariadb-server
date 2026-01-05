MariaDB/server

MDEV-31956 SSD based InnoDB buffer pool extension
In one of the practical cloud MariaDB setups, a server node accesses its
datadir over the network, but also has a fast local SSD storage for
temporary data. The content of such temporary storage is lost when the
server container is destroyed.

The commit uses this ephemeral fast local storage (SSD) as an extension of
the portion of InnoDB buffer pool (DRAM) that caches persistent data
pages. This cache is separated from the persistent storage of data files
and ib_logfile0 and ignored during backup.

The following system variables were introduced:

innodb_extended_buffer_pool_size - the size of external buffer pool
file, if it equals to 0, external buffer pool will not be used;

innodb_extended_buffer_pool_path - the path to external buffer pool
file.

If innodb_extended_buffer_pool_size is not equal to 0, external buffer
pool file will be (re)created on startup.

Only clean pages will be flushed to external buffer pool file. There is
no need to flush dirty pages, as such pages will become clean after
flushing, and then will be evicted when they reach the tail of LRU list.

The general idea of this commit is to flush clean pages to external
buffer pool file when they are evicted.

A page can be evicted eigther by transaction thread or by background
thread of page cleaner. In some cases transaction thread is waiting for
page cleaner thread to finish its job. We can't do flushing in external
buffer pool file when transaction threads are waithing for eviction,
that would heart performance. That's why the only case for flushing is
when page cleaner thread evicts pages in background and there are no
waiters. For this purprose buf_pool_t::done_flush_list_waiters_count
variable was introduced, we flush evicted clean pages only if the
variable is zeroed.

Clean pages are evicted in buf_flush_LRU_list_batch() to keep some
amount of pages in buffer pool's free list. That's why we flush every
second page to external buffer pool 

storage/innobase/buf/buf0lru.cc
@@ -104,10 +104,11 @@ uint	buf_LRU_old_threshold_ms;
 If !bpage->frame && bpage->oldest_modification() <= 1,
 the object will be freed.
 
-@param bpage      buffer block
-@param id         page identifier
-@param chain      locked buf_pool.page_hash chain (will be released here)
-@param zip        whether bpage->zip of BUF_BLOCK_FILE_PAGE should be freed
+@param bpage        buffer block
+@param id           page identifier
+@param chain        locked buf_pool.page_hash chain (will be released here)
+@param zip          whether bpage->zip of BUF_BLOCK_FILE_PAGE should be freed
+@param ext_buf_page external buffer page to replace bpage in page hash
 
 If a compressed page is freed other compressed pages may be relocated.
 @retval true if bpage with bpage->frame was removed from page_hash. The
@@ -116,7 +117,8 @@ caller needs to free the page to the free list
 this case the block is already returned to the buddy allocator. */
 static bool buf_LRU_block_remove_hashed(buf_page_t *bpage, const page_id_t id,
                                         buf_pool_t::hash_chain &chain,
-                                        bool zip);
+                                        bool zip,
+                                        ext_buf_page_t *ext_buf_page= nullptr);
 
 /** Free a block to buf_pool */
 static void buf_LRU_block_free_hashed_page(buf_block_t *block)
@@ -722,7 +724,8 @@ The caller must hold buf_pool.mutex.
 @param zip        whether to remove both copies of a ROW_FORMAT=COMPRESSED page
 @retval true if freed and buf_pool.mutex may have been temporarily released
 @retval false if the page was not freed */
-bool buf_LRU_free_page(buf_page_t *bpage, bool zip)
+bool buf_LRU_free_page(buf_page_t *bpage, bool zip,
+    ext_buf_page_t *ext_buf_page)
 {
 	const page_id_t id{bpage->id()};
 	buf_page_t*	b = nullptr;
@@ -807,7 +810,7 @@ func_exit:
 
 	ut_ad(bpage->can_relocate());
 
-	if (!buf_LRU_block_remove_hashed(bpage, id, chain, zip)) {
+	if (!buf_LRU_block_remove_hashed(bpage, id, chain, zip, ext_buf_page)) {
 		ut_ad(!b);
 		mysql_mutex_assert_not_owner(&buf_pool.flush_list_mutex);
 		return(true);
@@ -986,6 +989,7 @@ If !bpage->frame && !bpage->oldest_modification(), the object will be freed.
 @param id         page identifier
 @param chain      locked buf_pool.page_hash chain (will be released here)
 @param zip        whether bpage->zip of BUF_BLOCK_FILE_PAGE should be freed
+@param ext_buf_page external buffer page to replace bpage in page hash
 
 If a compressed page is freed other compressed pages may be relocated.
 @retval true if BUF_BLOCK_FILE_PAGE was removed from page_hash. The
@@ -994,7 +998,8 @@ caller needs to free the page to the free list
 this case the block is already returned to the buddy allocator. */
 static bool buf_LRU_block_remove_hashed(buf_page_t *bpage, const page_id_t id,
                                         buf_pool_t::hash_chain &chain,
-                                        bool zip)
+                                        bool zip,
+                                        ext_buf_page_t *ext_buf_page)
 {
 	ut_a(bpage->can_relocate());
 	ut_ad(buf_pool.page_hash.lock_get(chain).is_write_locked());
@@ -1058,7 +1063,16 @@ static bool buf_LRU_block_remove_hashed(buf_page_t *bpage, const page_id_t id,
 		MEM_CHECK_ADDRESSABLE(bpage->zip.data, bpage->zip_size());
 	}
 
-	buf_pool.page_hash.remove(chain, bpage);
+	if (ext_buf_page) {
+		buf_pool.push_ext_page_to_LRU(*ext_buf_page);
+		ut_ad(ext_buf_page->id == bpage->id());
+		ext_buf_page->hash= bpage->hash;
+		buf_pool.page_hash.replace(chain, bpage,
+		    reinterpret_cast<buf_page_t *>(ext_buf_page));
+	}
+	else
+		buf_pool.page_hash.remove(chain, bpage);
+
 	page_hash_latch& hash_lock = buf_pool.page_hash.lock_get(chain);
 
 	if (UNIV_UNLIKELY(!bpage->frame)) {

vlad-lesin/mariadb-server

storage/innobase/buf/buf0lru.cc
@@ -105,10 +105,11 @@ uint	buf_LRU_old_threshold_ms;
 If !bpage->frame && bpage->oldest_modification() <= 1,
 the object will be freed.
 
-@param bpage      buffer block
-@param id         page identifier
-@param chain      locked buf_pool.page_hash chain (will be released here)
-@param zip        whether bpage->zip of BUF_BLOCK_FILE_PAGE should be freed
+@param bpage        buffer block
+@param id           page identifier
+@param chain        locked buf_pool.page_hash chain (will be released here)
+@param zip          whether bpage->zip of BUF_BLOCK_FILE_PAGE should be freed
+@param ext_buf_page external buffer page to replace bpage in page hash
 
 If a compressed page is freed other compressed pages may be relocated.
 @retval true if bpage with bpage->frame was removed from page_hash. The
@@ -117,7 +118,8 @@ caller needs to free the page to the free list
 this case the block is already returned to the buddy allocator. */
 static bool buf_LRU_block_remove_hashed(buf_page_t *bpage, const page_id_t id,
                                         buf_pool_t::hash_chain &chain,
-                                        bool zip);
+                                        bool zip,
+                                        ext_buf_page_t *ext_buf_page= nullptr);
 
 /** Free a block to buf_pool */
 static void buf_LRU_block_free_hashed_page(buf_block_t *block)
@@ -736,7 +738,8 @@ The caller must hold buf_pool.mutex.
 @param zip        whether to remove both copies of a ROW_FORMAT=COMPRESSED page
 @retval true if freed and buf_pool.mutex may have been temporarily released
 @retval false if the page was not freed */
-bool buf_LRU_free_page(buf_page_t *bpage, bool zip)
+bool buf_LRU_free_page(buf_page_t *bpage, bool zip,
+    ext_buf_page_t *ext_buf_page)
 {
 	const page_id_t id{bpage->id()};
 	buf_page_t*	b = nullptr;
@@ -820,7 +823,7 @@ func_exit:
 
 	ut_ad(bpage->can_relocate());
 
-	if (!buf_LRU_block_remove_hashed(bpage, id, chain, zip)) {
+	if (!buf_LRU_block_remove_hashed(bpage, id, chain, zip, ext_buf_page)) {
 		ut_ad(!b);
 		mysql_mutex_assert_not_owner(&buf_pool.flush_list_mutex);
 		return(true);
@@ -993,7 +996,7 @@ ATTRIBUTE_COLD void buf_pool_t::free_block(buf_block_t *block) noexcept
   mysql_mutex_unlock(&mutex);
 }
 
-inline void
+void
 buf_pool_t::page_hash_table::remove(buf_pool_t::hash_chain &chain,
                                     buf_page_t *bpage) noexcept
 {
@@ -1019,6 +1022,7 @@ If !bpage->frame && !bpage->oldest_modification(), the object will be freed.
 @param id         page identifier
 @param chain      locked buf_pool.page_hash chain (will be released here)
 @param zip        whether bpage->zip of BUF_BLOCK_FILE_PAGE should be freed
+@param ext_buf_page external buffer page to replace bpage in page hash
 
 If a compressed page is freed other compressed pages may be relocated.
 @retval true if BUF_BLOCK_FILE_PAGE was removed from page_hash. The
@@ -1027,7 +1031,8 @@ caller needs to free the page to the free list
 this case the block is already returned to the buddy allocator. */
 static bool buf_LRU_block_remove_hashed(buf_page_t *bpage, const page_id_t id,
                                         buf_pool_t::hash_chain &chain,
-                                        bool zip)
+                                        bool zip,
+                                        ext_buf_page_t *ext_buf_page)
 {
 	ut_a(bpage->can_relocate());
 	ut_ad(buf_pool.page_hash.lock_get(chain).is_write_locked());
@@ -1091,7 +1096,16 @@ static bool buf_LRU_block_remove_hashed(buf_page_t *bpage, const page_id_t id,
 		MEM_CHECK_ADDRESSABLE(bpage->zip.data, bpage->zip_size());
 	}
 
-	buf_pool.page_hash.remove(chain, bpage);
+	if (ext_buf_page) {
+		buf_pool.push_ext_page_to_LRU(*ext_buf_page);
+		ut_ad(ext_buf_page->id_ == bpage->id());
+		ext_buf_page->hash= bpage->hash;
+		buf_pool.page_hash.replace(chain, bpage,
+		    reinterpret_cast<buf_page_t *>(ext_buf_page));
+	}
+	else
+		buf_pool.page_hash.remove(chain, bpage);
+
 	page_hash_latch& hash_lock = buf_pool.page_hash.lock_get(chain);
 
 	if (UNIV_UNLIKELY(!bpage->frame)) {